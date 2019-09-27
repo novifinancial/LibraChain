9d6ba66325dd85f1ef585326605d6cd059720600
François Garillot <francois@garillot.net>
2019-09-27 13:24:06 -0700

    Added a theory of conditionally-signed types.

--- .coqdeps.d
+++
@@ -1,6 +0,0 @@
-Structures/SeqFacts.vo Structures/SeqFacts.glob Structures/SeqFacts.v.beautified: Structures/SeqFacts.v
-Structures/SeqFacts.vio: Structures/SeqFacts.v
-Structures/Chains.vo Structures/Chains.glob Structures/Chains.v.beautified: Structures/Chains.v
-Structures/Chains.vio: Structures/Chains.v
-Structures/Blocks.vo Structures/Blocks.glob Structures/Blocks.v.beautified: Structures/Blocks.v Structures/SeqFacts.vo Structures/Chains.vo
-Structures/Blocks.vio: Structures/Blocks.v Structures/SeqFacts.vio Structures/Chains.vio
